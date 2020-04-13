34e09ce9fcb45039263d47c4372acc1e50f55278
arunkaliappan118 <akblog7@gmail.com>
2020-04-14 04:43:56 +0530

    comment

--- src/test/java/com/example/howtodoinjava/hellodocker/HelloDockerApplicationTests.java
+++ src/test/java/com/example/howtodoinjava/hellodocker/HelloDockerApplicationTests.java
@@ -1,16 +1,16 @@
-package com.example.howtodoinjava.hellodocker;
+// //package com.example.howtodoinjava.hellodocker;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class HelloDockerApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class HelloDockerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
 
-}
+// //}
